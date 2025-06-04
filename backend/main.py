21519468b11a52b00f9b911b69ba83535b34940e
cheuyin <yinstanleycheung@gmail.com>
2025-06-04 13:14:38 -0700

    Implement basic POST/GET functionality for creating short URLs and retrieving original URLs

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+url_store = {}  # Maps alias -> original URL
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world!"}
+base_url = "https://stanshorts.com/"
+
+
+class ShortURLRequest(BaseModel):
+    original_url: str
+    alias: str
+
+
+@app.get("/api/v1/urls/{alias}")
+async def get_original_url(alias: str):
+    # Check that the alias is stored
+    if alias not in url_store:
+        raise HTTPException(status_code=404, detail="URL doesn't exist")
+
+    return {"original_url": url_store[alias]}
+
+
+@app.post("/api/v1/urls/")
+async def create_short_url(request: ShortURLRequest):
+    short_url = base_url + request.alias
+    url_store[request.alias] = request.original_url
+    return {"short_url": short_url}
--- todos.md
+++ todos.md
@@ -1,6 +1,8 @@
 ## Step 1: Create a backend with all the basic features
 No database yet. All the shortened links are stored in memory.
-- [ ] Create a Python virtual env in `backend/`
-- [ ] Start a FastAPI server
+- [x] Create a Python virtual env in `backend/`
+- [x] Start a FastAPI server
 - [ ] Create a `POST` endpoint that saves a long URL --> short URL mapping in memory
 - [ ] Create a `GET` endpoint for short URLs and redirects the user if the mapping exists
+
+
